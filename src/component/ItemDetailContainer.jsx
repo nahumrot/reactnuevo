a57ac0638e8a775c02fc124e831e42e80d59adb8
nahumrud <nahumrud@hotmail.com>
2022-08-08 18:50:37 -0300

    img

--- src/component/ItemDetailContainer.jsx
+++ src/component/ItemDetailContainer.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import ItemDetail from '../component/ItemDetail';
+import imagen1 from '../imagenes/jbl'
 
 const producto = [{
   id:1,
   titulo: "Auricular JBL",
   precio: 9000,
   detalles: "La bateria dura 40 horas, tiene microfono incorporado y un sonido superior que lo hace unico.",
-  img: require.context('../imagenes/aurijblazul.jpg')
+  img: <img src={imagen1} alt="" srcset="" />
   
 }];
 
